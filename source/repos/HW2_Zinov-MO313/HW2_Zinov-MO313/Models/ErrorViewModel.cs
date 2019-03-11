5ae13f910dcf54a3de5b00c28bae89284907e26a
Vladislav <ufaaaaaaaa@gmail.com>
2019-03-11 20:07:45 +0500

    Фиксация

---
+++ source/repos/HW2_Zinov-MO313/HW2_Zinov-MO313/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW2_Zinov_MO313.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
